eccce34b184a5756c45c1640ccd379f4ec8282e9
cédric <cedric_lecalvez@hotmail.com>
2020-11-05 16:38:49 +0100

    homePage v1 ok

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,13 @@ var city = ["Paris","Marseille","Nantes","Lyon","Rennes","Melun","Bordeaux","Lil
 var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"]
 
 
+/* GET home page. */
+router.get('/homePage', function(req, res, next) {
+  
+  res.render('homePage', { title: 'Express' });
+});
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
