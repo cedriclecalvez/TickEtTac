b975b257e6c1794d949006c723bca2977d5697e8
cédric <cedric_lecalvez@hotmail.com>
2021-02-03 15:25:49 +0100

    first commit apres generation de bdd fictive

--- routes/index.js
+++ routes/index.js
@@ -153,3 +153,35 @@ if (searchTrip.length>0){
 
 
 module.exports = router;
+
+
+
+// // --------------------------JUST ONCE-------------------------------------
+
+// // Remplissage de la base de donnée, une fois suffit
+// router.get('/save', async function(req, res, next) {
+
+//   // How many journeys we want
+//   var count = 300
+
+//   // Save  ---------------------------------------------------
+//     for(var i = 0; i< count; i++){
+
+//     departureCity = city[Math.floor(Math.random() * Math.floor(city.length))]
+//     arrivalCity = city[Math.floor(Math.random() * Math.floor(city.length))]
+
+//     if(departureCity != arrivalCity){
+
+//       var newJourney = new journeyModel ({
+//         departure: departureCity , 
+//         arrival: arrivalCity, 
+//         date: date[Math.floor(Math.random() * Math.floor(date.length))],
+//         departureTime:Math.floor(Math.random() * Math.floor(23)) + ":00",
+//         price: Math.floor(Math.random() * Math.floor(125)) + 25,
+//       });
+//        await newJourney.save();
+//     }
+
+//   }
+//   res.render('index', { title: 'Express' });
+// });
